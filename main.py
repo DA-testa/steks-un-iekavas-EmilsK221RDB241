a57d3407d5997c11874b5860e7694f251470daa2
EmilsK221RDB241 <125762817+EmilsK221RDB241@users.noreply.github.com>
2023-02-19 18:00:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ def find_mismatch(text):
 
         if next in "([{":
            opening_brackets_stack.append(Bracket(next, i))
-        pass
            
 
-        if next in ")]}":
+        elif next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i + 1
             opening_brackets_stack.pop()
 
 
-        if opening_brackets_stack:
-            v = opening_brackets_stack[0].position
+        if not opening_brackets_stack:
         return "Success"
-        pass
+    else:
+        v = opening_brackets_stack[0].position
+        return v + 1
 
 
 def main():
@@ -37,6 +37,8 @@ def main():
     if text[0] == "I":
         text = input()
         mismatch = find_mismatch(text)
+        
+    print(mismatch)
 
-if __name__ == "__main__":
+if name == "main":
     main()
